319dd498d7f446e97046dc01343724c9f7181cef
benjamindz <dzaferovicbenjamin@gmail.com>
2021-04-02 19:47:34 +0200

    Added ex02

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestAsyncAwaits from './GetRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestAsyncAwaits />
+    </div>
+  );
+}
+
+export default App;
